f139aec3c5d9c33a585ec9c1eb0755d32c729361
SANJAY DESHPANDE <sanjay.deshpande2389@gmail.com>
2021-03-23 16:23:04 +0530

    Update retrieve_structures.py

--- retrieve_structures.py
+++ retrieve_structures.py
@@ -1,38 +1,18 @@
-import os
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import pubchempy as pcp
 import pandas as pd
 from collections import deque
-
-list_compounds = pd.read_csv("compounds.csv", header=None)
-
-data = list(list_compounds[0])
-len(data)
-
-Compounds = {}
-for i in data:
-        a = pcp.get_compounds(i, 'name')
-        Compounds[i] = a
-len(Compounds)
-
-no_result = [] 
-for i in (Compounds):
-    if (Compounds[i] == []):
-        no_result.append(i)
-    
-len(no_result)
-
-for i in deque(Compounds.keys()):  #Deque helps to remove elements from any part of the dictionary; remove the no_result IDs from original dictionary
-    for j in no_result:
-        if i == j:
-            del Compounds[i]
-
-len(Compounds)
-
-for i in Compounds:
+import sys
+data = pd.read_csv("compounds.csv", header=None)
+list_of_compounds = list(data[0])
+for i in list_of_compounds:
     try:
-        pcp.download('SDF', f'{i}.sdf', i, 'name', record_type = '3d' )
+        pcp.download('SDF', f'{i}.sdf', i, 'name', record_type = '3d')
     except:
-        print(i)
-
-
-no_result
+        with open('not_downloaded.txt', 'a') as f:
+            print(i, file=f)
